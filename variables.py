23f005a3f4b029e8c6d4e49138e6b816f2ca88b1
jorgegrajalesu <jegosystem@gmail.com>
2025-09-02 19:27:03 -0500

    first commit

---
+++ variables.py
@@ -0,0 +1,9 @@
+#como definir variables en python
+mi_nombre = "Jorge"
+print(mi_nombre)
+num1 = 5
+num2 = 4
+suma= num1 + num2
+print(num1, num2, mi_nombre, suma)
+
+nom = input("Cuál es tu nombre")
