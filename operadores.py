8b2612140e2777b0749354ae58b44944c0a9449c
jorgegrajalesu <jegosystem@gmail.com>
2025-09-02 20:14:03 -0500

    020925

--- operadores.py
+++ operadores.py
@@ -6,3 +6,35 @@ print("la resta es: ", 7 - 5)
 print("la multiplicación es: ", 7 * 5)
 #operadores de división
 print("la división es: ", 7 / 5)
+#operadores de mod
+print("El resto es: ", 7 % 5)
+#operadores de exponente
+print("El exponente es: ", 7 ** 5)
+
+#concatenar
+
+print("Mis nombres" + " " + "Jorge" + " " + "Enrique")
+
+#operadores con variables
+num1= 5
+num2 = 2
+suma =num1 + num2
+resta = num1 - num2
+mult = num1 * num2
+div = num1 / num2
+mod= num1 % num2
+exp= num1 ** num2
+
+print("la suma es: " , suma)
+print("la resta es: " , resta)
+print("la mult es: " , mult)
+print("la division es: " , div)
+print("la mod es: " , mod)
+print("la expo es: " , exp)
+
+#Operadores comparativos
+print(5 > 2)
+print(5 < 2)
+print(5 >= 2)
+print(5 == 2)
+print(5 != 2)
