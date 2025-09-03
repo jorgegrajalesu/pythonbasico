eef3f78be4aaeec95439e423c25c6efbd90c840a
jorgegrajalesu <jegosystem@gmail.com>
2025-09-02 19:40:07 -0500

    020925

---
+++ operadores.py
@@ -0,0 +1,8 @@
+#operadores de suma
+print("la suma es: ",7 + 5)
+#operadores de resta
+print("la resta es: ", 7 - 5)
+#operadores de multiplicación
+print("la multiplicación es: ", 7 * 5)
+#operadores de división
+print("la división es: ", 7 / 5)
