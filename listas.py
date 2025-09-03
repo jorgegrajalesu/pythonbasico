d3bd17a7d4366e6cc494c222730617cbaba67208
jorgegrajalesu <jegosystem@gmail.com>
2025-09-02 21:22:16 -0500

    020925

---
+++ listas.py
@@ -0,0 +1,7 @@
+#las listas, estructuran datos
+lista_mes = ["enero","febrero","marzo","abril","mayo","junio","julio","agosto","septiembre","octubre","noviembre","diciembre"]
+print(lista_mes)
+print(len(lista_mes))
+
+persona= ["Jorge", "Grajales", 48, "1976"]
+print(persona)
