53302d4bd01514546f3122dd99ad1d5ba5f648b2
jorgegrajalesu <jegosystem@gmail.com>
2025-09-08 21:35:09 -0500

    080925

--- diccionarios.py
+++ diccionarios.py
@@ -1,3 +1,13 @@
 #diccionario y sets se definen igual
 persona = {"Nombre":"Jorge","Apellido":"Grajales", "Edad":49, 1:"php"}
 print(persona)
+
+programas = {
+    "Lenguajes": {"HTML", "Python", "C#"},
+    "bases_de_datos":{"postgress", "sql server", "mysql"},
+    
+}
+
+print(programas)
+#para acceder a los diccionarios
+print(persona["Nombre"])
