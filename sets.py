e71aa35614b90aaa0050914c684070656522e89e
jorgegrajalesu <jegosystem@gmail.com>
2025-09-08 20:59:19 -0500

    080925

--- sets.py
+++ sets.py
@@ -10,3 +10,12 @@ print(persona)#no admite repetidos
 #no se puede acceder como en las listas y tuplas
 #forma correcta de comprobar que un elemento existe en un set es con in
 print(49 in persona)
+#eliminar datos
+persona.remove("Usuga")
+print(persona)
+persona.clear()
+print(persona)
+persona.add("Usuga")
+persona.add("Jorge")
+persona.add(45)
+print(persona)
