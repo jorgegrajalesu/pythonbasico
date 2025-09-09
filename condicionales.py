9a46cb293b45241ae32df7ea02c30de850e60ae8
jorgegrajalesu <jegosystem@gmail.com>
2025-09-08 22:35:12 -0500

    080925

--- condicionales.py
+++ condicionales.py
@@ -3,5 +3,14 @@ edad=int(input("Cual es tu edad? "))
 
 if edad>=18:
     print ("Mayor de edad")
+
+else:
+    print("Menor de edad")
+
+#condicion anidada o con el elif
+if edad>=18 and edad<=59:
+    print ("Mayor de edad")
+elif edad>=60 and edad>=90:
+    print("Adulto mayor")
 else:
     print("Menor de edad")
