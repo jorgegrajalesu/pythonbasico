e81ad0427a7f0b8f66220d023ddff82caa952a75
jorgegrajalesu <jegosystem@gmail.com>
2025-09-11 20:23:34 -0500

    110925

--- loops.py
+++ loops.py
@@ -4,3 +4,19 @@ num = 0
 while num < 10 :
     print(num)
     num += 1
+else:
+    print(num, "es mayor o igual")
+print("el programa sigue")
+
+#for
+persona= ["Jorge", "Grajales", 48, "1976"]
+for elemento in persona:
+    print(elemento)
+print("continua programa")
+
+lista = [[56, 34, 1],
+         [12, 4, 5],
+         [9, 4, 3]]
+for i in lista:
+    for j in i:
+        print(j)
